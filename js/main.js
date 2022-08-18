6e07c72c02d6f79b8c6fd3443ec8a35d3bad5be1
BORETS2002 <BORETS2002@example.com>
2022-08-18 18:06:43 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let elboy = document.querySelector(".body");
+let toggleNight = document.querySelector(".butoon");
+toggleNight.addEventListener("click", function () {
+  elboy.classList.toggle("body-night");
+});
